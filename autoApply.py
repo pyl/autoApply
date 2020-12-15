5aa41529ae4630af8717dd653b300a9486e21199
python8u <python8u@gmail.com>
2020-12-15 18:26:41 -0500

    dictionary of links

--- autoApply.py
+++ autoApply.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import pprint
+from collections import OrderedDict
 
 url = "https://github.com/pittcsc/Summer2021-Internships"
 
@@ -7,9 +9,23 @@ source = requests.get(url).text
 
 soup = BeautifulSoup(source, 'lxml')
 
-
+table = soup.table
 #print(soup.prettify) contains no tables but print(soup.table) 
 # works somehow 
+rows = table.find_all('tr')
+rows.pop(0)
 
+links = []
 
-print(soup.table.prettify())
+linkdict = OrderedDict()
+for x in rows:
+    if x.td.a is not None:
+        try:
+            linkdict[x.td.a.text] = x.td.a.get('href')
+        except:
+            print("ERROR" + x.td.a)
+    else:
+        linkdict[x.td.text] = None
+
+collections.OrderedDict(reversed(list(tempdict.items())))
+pprint.pprint(linkdict)
